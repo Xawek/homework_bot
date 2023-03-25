e7eb8d1f4c3c002feb97a5787e8374d117241b65
Anton Bogdanov <xawek@yandex.ru>
2023-03-25 09:10:01 +0500

    Telegram_bot

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # homework_bot
 python telegram bot
+
+# Перед запуском бота \ Before runing bot
+    1. Создать и активировать виртуальное окружение \ Create and activate virtual environment
+        Создать \ Create
+
+            'python -m venv venv' - для \ for - Windows
+            'python3 -m venv venv' - для \ for - Linux
+
+        Активировать \ Activate
+
+            'source venv/Scripts/activate' - для \ for - Windows
+            'source venv/Bin/activate' - для \ for - Linux
+
+    2. Установить модули и пакеты для проекта из файла requirements.txt \ Install modules and packages for project from file requirements.txt
+        Установка \ Installation
+
+            'pip install requirements.txt' - для \ for - Windows
+            'pip3 install requirements.txt' - для \ for - Linux
+
+    3. В директории бота создать файл '.env' (homework_bot\.env) \ Create file '.env' in the bot directory (homework_bot\.env)
+        Создать фаил \ Create file
+
+            'touch .env'
+
+    4. В файле .env записать токены и id чата \ In .env file, write tokens and chat id
+        Пример \ Example
+
+            PRACTICUM_TOKEN=123_qWerTY
+            TELEGRAM_TOKEN=123:QweRTy
+            TELEGRAM_CHAT_ID=12345
+
+        Уточнение \ More detailed
+
+            PRACTICUM_TOKEN=<Токен_Яндекса\Yandex_Token>
+            TELEGRAM_TOKEN=<Токен_Бота_Телеграмма\Telegram_Bot_Token>
+            TELEGRAM_CHAT_ID=<id_Чата_Телеграмма\Telegram_Chat_id>
+
+
+    Пункт номер следующий написан не будет. Так как пишу и думаю о том что в лучшем случае это прочтёт человек 5 и 99% шанс того что они умеют читать по русски, ну и тем более они понимают что тут и как делается ^_^
--- homework.py
+++ homework.py
@@ -1,11 +1,26 @@
-...
+import logging
+import os
+import time
+from http import HTTPStatus
+
+import requests
+import telegram
+from dotenv import load_dotenv
 
 load_dotenv()
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='bot.log',
+    filemode='w',
+    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s',
+    encoding='utf-8'
+)
+logger.addHandler(logging.StreamHandler())
 
-
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -20,46 +35,90 @@ HOMEWORK_VERDICTS = {
 
 
 def check_tokens():
-    ...
+    """Проверка токенов и id чата."""
+    logger.debug('Проверка токенов и id чата.')
+    return all((PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID))
 
 
 def send_message(bot, message):
-    ...
+    """Отправка сообщения в Telegram чат."""
+    logger.debug('Отправка сообщения.')
+    try:
+        bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+    except Exception:
+        logger.error('Проблема с отправкой сообщения.')
+    else:
+        logger.debug('Сообщение отправлено.')
 
 
 def get_api_answer(timestamp):
-    ...
+    """Запрос к эндпоинту."""
+    logger.debug('Отправка запроса.')
+    payload = {'from_date': timestamp}
+    try:
+        homework_status = requests.get(
+            ENDPOINT, headers=HEADERS, params=payload)
+    except Exception:
+        logger.error('Ошибка запроса к API.')
+    else:
+        if homework_status.status_code != HTTPStatus.OK:
+            raise requests.HTTPError('Нет ответа на запрос к API.',)
+        return homework_status.json()
 
 
 def check_response(response):
-    ...
+    """Проверка ответа API."""
+    logger.debug('Проверка ответа API.')
+    if 'homeworks' not in response:
+        logger.error('В ответе API не содержится "homeworks".')
+        raise TypeError()
+    if not isinstance(response, dict):
+        logger.error('В ответе API не содержится словарь.')
+        raise TypeError()
+    homeworks = response.get('homeworks')
+    if not isinstance(homeworks, list):
+        logger.error('В ответе API не содержится список.')
+        raise TypeError()
+    return homeworks
 
 
 def parse_status(homework):
-    ...
-
+    """Извлечение точной информации о статусе домашней работы."""
+    logger.debug('Извлечение информации')
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
+    if 'homework_name' not in homework:
+        raise KeyError('Ответ не содержит homework_name.')
+    if homework_status not in HOMEWORK_VERDICTS:
+        raise ValueError(f'Ответ не содержит - {homework_status}')
+    verdict = HOMEWORK_VERDICTS[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def main():
     """Основная логика работы бота."""
-
-    ...
-
+    if not check_tokens():
+        logger.critical('Проблема проверки токенов и id чата.')
+        exit()
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
-
-    ...
-
+    prev_message = ''
     while True:
         try:
-
-            ...
-
+            response = get_api_answer(timestamp)
+            homework = check_response(response)
+            if homework:
+                message = parse_status(homework[0])
+                if prev_message != message:
+                    send_message(bot, message)
+                    prev_message = message
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
-        ...
+            if prev_message != message:
+                send_message(bot, message)
+                prev_message = message
+        finally:
+            time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
